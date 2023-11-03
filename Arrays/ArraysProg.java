ec1776f4caac420c0df8d69e3947f6b1e91f9612
satheshj <satheshjr6@gmail.com>
2023-11-03 20:21:32 +0530

    Arrays

---
+++ Arrays/ArraysProg.java
@@ -0,0 +1,21 @@
+package FullStack.Arrays;
+
+import java.util.Arrays;
+
+public class ArraysProg {
+	public static void main(String args[]) {
+		int numArray[] = {1,2,3,4};
+		//accessing individual elements
+		System.out.println(numArray[1]+" "+numArray[3]);
+		//finding length
+		System.out.println(numArray.length);
+		//for each  - used to iterate by values of the array for(variable : array){statement} 
+		String[] cars = {"Toyota","BMW","Lexus"}; 
+		for(String s : cars) {
+			System.out.println(s);
+		}
+		//Simple way to print array
+		System.out.println(Arrays.toString(numArray)); //[1, 2, 3, 4]
+	}
+
+}
---
+++ Arrays/MultiDimensionalArray.java
@@ -0,0 +1,36 @@
+package FullStack.Arrays;
+
+import java.util.Scanner;
+
+public class MultiDimensionalArray {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		//matrix addition
+		int a[][] = new int[2][1];
+		int b[][] = new int[2][1];
+		int res[][] = new int [2][1];
+		System.out.println("Enter matrix");
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a[i].length; j++) { //a[i].length - a[i] is an array
+				a[i][j] = sc.nextInt();
+				
+			}
+		}
+		System.out.println("Enter matrix");
+		for(int i = 0; i < b.length; i++) {
+			for(int j = 0; j < b[i].length; j++) { 
+				b[i][j] = sc.nextInt();
+				
+			}
+		}
+		for(int i = 0; i < res.length; i++) {
+			for(int j = 0; j < res[i].length; j++) { //a[i].length - a[i] is an array
+				res[i][j] = a[i][j] + b[i][j];
+				System.out.print(res[i][j]);
+				
+			}
+			System.out.println();
+		}
+		
+	}
+}
